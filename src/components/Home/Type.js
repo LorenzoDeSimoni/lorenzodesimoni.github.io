51dfade9075fc3374113a174d503519894d5a5c0
Lorenzo De Simoni <lorenzodesimoni16@gmail.com>
2025-05-22 18:55:59 +0200

    Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Data Scientist",
+          "Statistician",
+          "Specializing in AI",
         ],
         autoStart: true,
         loop: true,
