0073ef1464ed5472b3a3934c7976b6e7a5b7b656
Lorenzo De Simoni <lorenzodesimoni16@gmail.com>
2025-05-22 18:46:16 +0200

    Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,31 +19,30 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I fell in love with Statistics and Data Science 🤷‍♂️
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Python, RStudio and SAS. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">statistical models and Machine learning algorithms </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  data analysis.
                 </b>
               </i>
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className="purple">Java</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  doing reports.
                 </b>
               </i>
               &nbsp; like
